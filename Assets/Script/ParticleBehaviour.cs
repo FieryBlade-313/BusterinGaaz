a307190a4f00b52eb5559049a48aac0d9c1dcb91
FieryBlade-313 <mohdfauz.s18@iiits.in>
2021-07-17 12:26:08 +0530

    Added a Particle System

---
+++ Assets/Script/ParticleBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBehaviour : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        Destroy(collision.gameObject);
+    }
+}
