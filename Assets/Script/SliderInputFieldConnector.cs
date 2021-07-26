268f21bcc13a554f1126729f3fc9c9f42baa7b3f
FieryBlade-313 <mohdfauz.s18@iiits.in>
2021-07-26 17:49:20 +0530

    Connected Input field value with sliders

---
+++ Assets/Script/SliderInputFieldConnector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderInputFieldConnector : MonoBehaviour
+{
+    Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void UpdateValue(string value)
+    {
+        try
+        {
+            slider.value = float.Parse(value);
+        }
+        catch
+        {
+            Debug.Log("Not a number");
+        }
+    }
+
+    
+}
